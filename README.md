1ffcb107b467d6bfdfaca07ed5f14c5ccef53657
ShanRB <shanrb119@gmail.com>
2023-04-18 23:56:54 +1000

    adjust which key and telescope

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ## TODO 
 - treesitter setup 
 - lazy vim dependencies 
+## NOTE
+- Telescope requires to install ripgrep in your system, check https://github.com/BurntSushi/ripgrep
--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -3,6 +3,10 @@ return {
 	{ "nvim-telescope/telescope.nvim",
         tag = "0.1.1",
         lazy = true,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        }
     },
 	{ "nvim-telescope/telescope-media-files.nvim" },
+    { "nvim-telescope/telescope-fzy-native.nvim" },
 }
